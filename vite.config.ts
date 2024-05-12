f952229a5bf1ddb53358a0c390b787b5385079c5
Brayden Carlson <braycarlson@hotmail.com>
2024-05-11 21:58:42 -0600

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
     repo: 'https://github.com/braycarlson/braycarlson.github.io/',
     plugins: [react()],
 })
