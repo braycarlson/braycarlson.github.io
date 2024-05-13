a49dd6fbb119d5dd834a2d7d95875ea54944fe51
Brayden Carlson <braycarlson@hotmail.com>
2024-05-12 23:24:01 -0600

    Updates

---
+++ assets/index-DcDTgm87.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/StarDrawer-DpC0Ebpn.js","assets/index-Dv_lc2z2.js","assets/index-CxSx_Eve.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./index-Dv_lc2z2.js";async function _(a,t=!0){const{StarDrawer:r}=await e(()=>import("./StarDrawer-DpC0Ebpn.js"),__vite__mapDeps([0,1,2]));await a.addShape("star",new r,t)}export{_ as loadStarShape};
